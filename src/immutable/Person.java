b3097b3e5d0dd3cd0b36cea76e5fec450f6ab116
li1165631719 <li1165631719@users.noreply.github.com>
2024-06-24 09:47:32 +0800

    final不变性测试

---
+++ src/immutable/Person.java
@@ -0,0 +1,16 @@
+package immutable;
+
+/**
+ * 描述：     不可变的对象，演示其他类无法修改这个对象，public也不行
+ */
+public class Person {
+
+    final int age = 18;
+    String alice = new String("Alice");
+    final String name = alice;
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.alice = "44";
+        System.out.println(person.name);
+    }
+}
---
+++ src/immutable/TestFinal.java
@@ -0,0 +1,13 @@
+package immutable;
+
+/**
+ * 描述： 测试final能否被修改
+ *
+ * @author 李志豪
+ * @create 2024/6/24
+ */
+public class TestFinal {
+    public static void main(String[] args) {
+
+    }
+}
