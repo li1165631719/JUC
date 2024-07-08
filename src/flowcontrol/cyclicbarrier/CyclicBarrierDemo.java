07db03aeb045ce68925f1e4c8b319e08cb485c9e
li1165631719 <li1165631719@users.noreply.github.com>
2024-07-08 15:14:00 +0800

    演示CyclicBarrier

--- src/flowcontrol/cyclicbarrier/CyclicBarrierDemo.java
+++ src/flowcontrol/cyclicbarrier/CyclicBarrierDemo.java
@@ -1,5 +1,8 @@
 package flowcontrol.cyclicbarrier;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 /**
  * 描述：演示CyclicBarrier
  * @Author 李志豪
@@ -7,4 +10,42 @@ package flowcontrol.cyclicbarrier;
  */
 public class CyclicBarrierDemo {
 
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("所有人都到场了， 大家统一出发！");
+            }
+        });
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Task(i,cyclicBarrier)).start();
+        }
+    }
+
+    static class Task implements Runnable{
+
+        private int id;
+
+        private CyclicBarrier cyclicBarrier;
+
+        public Task(int id, CyclicBarrier cyclicBarrier) {
+            this.id = id;
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程"+id+"正在前往集合地点");
+            try {
+                Thread.sleep((long)Math.random()*10000);
+                System.out.println("线程"+id+"到了集合地点，开始等待其他人到达");
+                cyclicBarrier.await();
+                System.out.println("线程"+id+"出发了");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
